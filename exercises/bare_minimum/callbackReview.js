8467b71e5351a7b5933148fef6862df280de6d85
chadam189 <chadam189@gmail.com>
2017-11-01 13:22:36 -0700

    First attempt at promises tests

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,53 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
-};
+var pluckFirstLineFromFile = function (filePath, callback) {
+
+  fs.readFile(filePath, 'utf8', function (err, results) {
+
+    var rs = fs.createReadStream(filePath, {encoding: 'utf8'});
+    var acc = '';
+    var pos = 0;
+    var index;
+    rs
+      .on('data', function (chunk) {
+        index = chunk.indexOf('\n');
+        acc += chunk;
+        index !== -1 ? rs.close() : pos += chunk.length;
+      })
+      .on('close', function () {
+        results = acc.slice(0, pos + index);
+        callback(null, results);
+      })
+      .on('error', function () {
+        callback(err);
+      });
+  });
+}; 
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  
+  //get status code
+    // var statusCode = someFunction(url);
+  // if (statusCode === bad or undefined or null) {
+  //   callback(err);
+  // } else {
+  //   callback(null, statusCode);
+  // }
+  
+  $.ajax({
+    url: url, 
+    type: 'GET',
+    // data: {},
+    success: function (data, status) {
+      callback(null, status);
+    }, 
+    error: function (err) {
+      callback(err);
+    }
+  });
+  
 };
 
 // Export these functions so we can test them and reuse them in later exercises
